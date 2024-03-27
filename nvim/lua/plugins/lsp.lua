c759187e460c660d67c0a143b07ecfa205681d1a
pipopo <puchi.maru.milk@gmail.com>
2024-03-27 13:38:22 +0900

    edit lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,5 +3,18 @@ return {
 		name = "nvim-lspconfig",
 		dir = "@nvim_lspconfig@",
 		event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local lspconfig = require("lspconfig")
+            -- Lua
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+					},
+				},
+			})
+			-- Nix
+			lspconfig.nil_ls.setup({})
+        end,
 	},
 }
